05509c5d6a2e6a4426fb538ed4cf09c5dd5dfd5f
43Clock <luiscarlosm2000@gmail.com>
2022-04-18 14:03:49 +0100

    Something

--- TP2/raft.py
+++ TP2/raft.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
-import imp
 import logging
 from concurrent.futures import ThreadPoolExecutor
 from os import stat
 from re import S
-from tkinter.messagebox import NO
+import time
+import threading
 
 from numpy import mat
 from ms import send, receiveAll, reply, exitOnError
@@ -15,6 +15,7 @@ import logging
 
 class State():
     def __init__(self,node_ids):
+        self.currentState = 0 #0 - follower, 1-candidate, 2-leader
         self.currentTerm = 1
         self.votedFor = None
         self.log = []
@@ -48,8 +49,33 @@ logging.getLogger().setLevel(logging.DEBUG)
 executor=ThreadPoolExecutor(max_workers=1)
 
 dic = dict()
-
+TIMEOUT_TIME = 2
+clock = TIMEOUT_TIME
 #PrevLogIndex esta no nextIndex
+
+def decreaseTime():
+    global clock
+    while True:
+        time.sleep(1)
+        clock -= 1
+        logging.info(clock)
+
+
+def timeout():
+    global clock,state, node_id, node_ids
+    while True:
+        if clock < 0:
+            clock = TIMEOUT_TIME
+            state.currentState = 1
+            state.currentTerm += 1
+            clock = TIMEOUT_TIME
+            for node in node_ids:
+                if node != node_id:
+                    send(node_id,node,type="RRPC",term=state.term,candidateId=node_id,
+                         lastLogIndex=len(state.log)-1,
+                         lastLogTerm=-1 if len(state.log) == 0 else state.log[len(state.log)-1][1])
+                    
+
 
 def apply(msg):
     if msg.body.type == "write":
@@ -78,26 +104,27 @@ def reply_cliente(msg):
             reply(msg,type="read_ok",value=dic[msg.body.key])
 
 
-
 def handle(msg):
     #State
-    global node_id, node_ids, state, leader, dic
+    global node_id, node_ids, state, leader, dic, clock
     #Message Handlers
     
     if msg.body.type == 'init':
         node_id = msg.body.node_id
         node_ids = msg.body.node_ids
         logging.info('node %s initialized', node_id)
-        leader = node_ids[0]
         state = State(node_ids)
-        # if node_id == leader:
+        #leader = node_ids[0]
+        threading.Thread(target=decreaseTime).start()
+        threading.Thread(target=timeout).start()
+        # if state.currentState == 2:
         #     state.commitIndex = 0
         # for node in node_ids[1:]:
         #     send(node_id,node,type="ARPC",term=state.currentTerm, leaderId=leader,
         #          prevLogIndex=0,prevLogTerm=0,entries=[],leaderCommit=state.commitIndex)
         reply(msg,type='init_ok')
         
-    elif node_id == leader and msg.src not in node_ids:
+    elif state.currentState == 2 and msg.src not in node_ids:
         state.appendLog(msg)
         for node in node_ids:
             if node != leader:
@@ -107,10 +134,11 @@ def handle(msg):
                    entries=state.log[state.nextIndex[node]:],leaderCommit=state.commitIndex) 
         
 
-    elif node_id != leader and msg.src not in node_ids:
+    elif state.currentState != 2 and msg.src not in node_ids:
         reply(msg,type="error",code=11,text="not leader")
 
-    elif node_id != leader and msg.body.type == "ARPC":
+    elif state.currentState != 2 and msg.body.type == "ARPC":
+        clock = TIMEOUT_TIME
         if msg.body.term < state.currentTerm:
             logging.info(f"ERROR: TERM")
         elif state.log != [] and state.log[msg.body.prevLogIndex-1][1] != msg.body.prevLogTerm:
@@ -135,7 +163,7 @@ def handle(msg):
         logging.info(dic)
 
 
-    elif node_id == leader and msg.body.type == "ARPC_RESP":
+    elif state.currentState == 2 and msg.body.type == "ARPC_RESP":
         state.nextIndex[msg.src] = msg.body.nextIndex
         state.matchIndex[msg.src] = msg.body.matchIndex
         n = state.biggestMatch()
@@ -147,10 +175,11 @@ def handle(msg):
                     state.lastApplied += 1
         logging.info(dic)
 
+    elif state.currentState == 0 and msg.body.type == "RRPC":
+        #@TODO Continuar aqui
 
 # Main loop
 executor.map(lambda msg: exitOnError(handle, msg), receiveAll())
-
 
 # Visto que para todos os servidores, incluindo o leader, o commitIndex começa em 0, tal como o lastAplied que tambem começa em 0, entao quando este enviar um AppendEntries RPC aos seguidores, enviará o leaderCommit tambem a 0, visto que é o valor que tem inicialmente, ou seja 0, a condição do ponto 5. (leaderCommit > commitIndex) nunca irá ser verdadeira, ou seja os seguidores nunca irão alterar o commitIndex.
 # Da mesma forma
