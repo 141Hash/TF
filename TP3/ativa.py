b3acd77fe177ecdd78a2428cd012ce6077957b7b
43Clock <luiscarlosm2000@gmail.com>
2022-05-27 22:51:37 +0100

    TP3

---
+++ TP3/ativa.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+import logging
+from asyncio import run, create_task, sleep
+
+from ams import send, receiveAll, reply
+from db import DB
+
+logging.getLogger().setLevel(logging.DEBUG)
+
+db = DB()
+
+async def handle(msg):
+    # State
+    global node_id, node_ids
+    global db
+
+    # Message handlers
+    if msg.body.type == 'init':
+        node_id = msg.body.node_id
+        node_ids = msg.body.node_ids
+        logging.info('node %s initialized', node_id)
+
+        reply(msg, type='init_ok')
+
+    elif msg.body.type == 'txn':
+        logging.info('executing txn')
+        if msg.src not in node_ids:
+            logging.info("RE_SENDING")
+            for node in node_ids:
+                logging.info(msg.body.__dict__)
+                send(node_id,node,msg.body.__dict__)
+        else:
+            ctx = await db.begin([k for op,k,v in msg.body.txn], msg.src+'-'+str(msg.body.msg_id))
+            rs,wv,res = await db.execute(ctx, msg.body.txn)
+            if res:
+                await db.commit(ctx, wv)
+                reply(msg, type='txn_ok', txn=res)
+            else:
+                reply(msg, type='error', code=14, text='transaction aborted')
+            db.cleanup(ctx)
+
+    else:
+        logging.warning('unknown message type %s', msg.body.type)
+
+# Main loop
+run(receiveAll(handle))
