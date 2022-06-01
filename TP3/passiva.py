aab2c4b291dd1c2554d91ab57275927203997fb6
141Hash <accmartins3000@gmail.com>
2022-06-01 17:44:43 +0100

    Ativa quase feita

--- TP3/passiva.py
+++ TP3/passiva.py
@@ -3,8 +3,6 @@
 from http import client
 import logging
 from asyncio import run, create_task, sleep
-from multiprocessing.connection import wait
-from wsgiref.headers import tspecials
 
 from ams import send, receiveAll, reply
 from db import DB
@@ -17,15 +15,11 @@ async def handle(msg):
     # State
     global node_id, node_ids
     global db
-    global ts, queue, waitingList
 
     # Message handlers
     if msg.body.type == 'init':
         node_id = msg.body.node_id
         node_ids = msg.body.node_ids
-        ts = 0
-        queue = []
-        waitingList = []
         logging.info('node %s initialized', node_id)
 
         reply(msg, type='init_ok')
@@ -33,10 +27,10 @@ async def handle(msg):
     
     elif msg.body.type == 'txn':
         if msg.src not in node_ids:
-            send(node_id, "lin-tso" ,type='ts')
-            queue.append(msg)
-
+            for node in node_ids:
+                send(node_id, node, type = 'txn', txn = msg.body.txn, mensagem = msg.body.msg_id, client = msg)
         else:
+            send(node_id, "lin-tso" ,type='ts')
             ctx = await db.begin([k for op,k,v in msg.body.txn], msg.src+'-'+str(msg.body.mensagem))
             rs,wv,res = await db.execute(ctx, msg.body.txn)
             if res:
@@ -44,18 +38,6 @@ async def handle(msg):
                 reply(msg, type='txn_replicated_ok', txn=res, client = msg.body.client)
             else:
                 reply(msg, type='error', code=14, text='transaction aborted')
-            
-            ts += 1
-            
-            if len(waitingList) > 0:
-                for element in waitingList:
-                    if element[1] == ts:
-                        for node in node_ids:
-                            send(node_id, node, type = 'txn', txn = element[0].body.txn, mensagem = element[0].body.msg_id, client = element[0])
-
-                        waitingList.remove(element)
-                        break
-
             db.cleanup(ctx)
 
     elif msg.body.type == 'txn_replicated_ok':
@@ -63,17 +45,6 @@ async def handle(msg):
 
     elif msg.body.type == 'ts_ok':
         logging.info('TS: %s', msg.body.ts)
-        req = queue.pop(0)
-        waitingList.append((req, msg.body.ts))
-
-        for element in waitingList:
-            if element[1] == ts:
-                for node in node_ids:
-                    send(node_id, node, type = 'txn', txn = element[0].body.txn, mensagem = element[0].body.msg_id, client = element[0])
-
-                waitingList.remove(element)
-                break
-
         
     else:
         logging.warning('unknown message type %s', msg.body.type)
